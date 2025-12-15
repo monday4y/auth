02ee1184eb9cdf289f41985e67b11d7e232e086f
PRIYANSHU KESTWAL <priyanshukestwal67@gmail.com>
2025-12-15 15:43:02 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 const connectDB=async()=>{
     try{
-       await mongoose.connect("mongodb+srv://priyanshukestwal67_db_user:Rv0G8lvfROieBGuC@cluster0.xt1qarx.mongodb.net/?appName=Cluster0");
+       await mongoose.connect("your_string");
       console.log("MongoDB Connected")
     }
     catch(error){
